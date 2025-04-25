474ee9f204a078a7dfa794a41aad3cbaf3c80a65
Krushit Kaneriya <krushitkaneriya@gmail.com>
2025-04-25 12:09:13 +0000

    strings done

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,38 @@
+const name = "krushit";
+const repoCount = 6;
+
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Krushit-kaneriya-anilkumar");
+
+// console.log(gameName[0]);
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("h"));
+
+// const newString = gameName.substring(0, 4);
+// console.log(newString);
+
+// const anotherString = gameName.slice(-7, 2);
+// console.log(anotherString);
+
+// const newStringOne = "   krushit   ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+const url = "https://krushitkaneriya774.github.io";
+// console.log(url.replace("https://", ""));
+// console.log(url.includes("krushit"));
+
+// console.log(gameName.split("-"));
+
