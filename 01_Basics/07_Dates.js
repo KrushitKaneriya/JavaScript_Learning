e953d1036b4b002b3932450b9f3f8dfe83f7be82
Krushit Kaneriya <krushitkaneriya@gmail.com>
2025-04-29 08:38:06 +0000

    Nums_Maths_Dates Done

---
+++ 01_Basics/07_Dates.js
@@ -0,0 +1,38 @@
+// Dates
+
+let myDate = new Date();
+// console.log(typeof myDate);
+
+
+// console.log(myDate); // 2025-04-29T08:15:59.983Z
+// console.log(myDate.toString()); // Tue Apr 29 2025 08:16:54 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString()); // Tue Apr 29 2025
+// console.log(myDate.toLocaleString()); // 4/29/2025, 8:16:54 AM
+// console.log(myDate.toLocaleDateString()); // 4/29/2025
+// console.log(myDate.toISOString()); // 2025-04-29T08:16:54.983Z
+// console.log(myDate.toJSON()); // 2025-04-29T08:16:54.983Z
+
+
+// let myCreatedDate = new Date(2025, 0, 24);
+// let myCreatedDate = new Date(2025, 0, 24, 5, 3, 2);
+// let myCreatedDate = new Date("2025-01-24");
+let myCreatedDate = new Date("01-24-2025");
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now() / 1000));
+
+
+let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+// console.log(newDate.getFullYear());
+// console.log(newDate.getHours());
+// console.log(newDate.getMinutes());
+// console.log(newDate.getSeconds());
+// console.log(newDate.getMilliseconds());
+// console.log(newDate.getTime());
+
